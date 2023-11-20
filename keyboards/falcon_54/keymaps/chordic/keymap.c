9199c34a5b6261ed4b7864e835ea876b315e92c2
WMixter <57375803+WMixter20@users.noreply.github.com>
2023-11-19 20:32:20 -0600

    updated chordic  keymap

--- keyboards/falcon_54/keymaps/chordic/keymap.c
+++ keyboards/falcon_54/keymaps/chordic/keymap.c
@@ -7,12 +7,14 @@ enum wm_keycodes {
     KC_CLEAD = SAFE_RANGE,
     KC_ENC_TOG,
     TD_ESC_TILD = 0,
+    TD_TOG_TILD = 0,
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_ESC_TILD] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV),
+    [TD_TOG_TILD] = ACTION_TAP_DANCE_DOUBLE(KC_ENC_TOG, KC_MNXT),
 };
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
@@ -22,10 +24,10 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { 
 
     [_BASE] = LAYOUT_split_5x6(
-        TD(TD_ESC_TILD),         KC_1,         KC_2,          KC_3,         KC_4,         KC_5,                                         KC_CLEAD,        KC_6,      KC_7,         KC_8,         KC_9,      KC_0,                           
-        KC_TAB,         KC_Q,         KC_W,          KC_E,         KC_R,         KC_T,                                         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,      KC_MPLY,                              
-        KC_CAPS,        KC_A,         KC_S,          KC_D,         KC_F,         KC_G,                                         KC_H,         KC_J,         KC_K,         KC_UP,        KC_L,      KC_MUTE,                              
-                        KC_Z,         KC_X,          KC_C,         KC_V,         KC_B,        KC_ENC_TOG,                         KC_N,         KC_M,         KC_LEFT,     KC_DOWN,       KC_RIGHT,      
+        TD(TD_ESC_TILD),         KC_1,         KC_2,          KC_3,         KC_4,         KC_5,                                KC_CLEAD,        KC_6,      KC_7,         KC_8,         KC_9,      KC_0,                           
+        KC_TAB,                  KC_Q,         KC_W,          KC_E,         KC_R,         KC_T,                                         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,      KC_MPLY,                              
+        KC_LSFT,                 KC_A,         KC_S,          KC_D,         KC_F,         KC_G,                                         KC_H,         KC_J,         KC_K,         KC_UP,        KC_L,      KC_MUTE,                              
+                                 KC_Z,         KC_X,          KC_C,         KC_V,         KC_B,        TD(TD_TOG_TILD),                 KC_N,         KC_M,         KC_LEFT,     KC_DOWN,       KC_RIGHT,      
                                                      KC_LCTL,        KC_LALT,      KC_LCMD,      KC_SPC,            KC_BSPC,   KC_RCMD,       KC_ENT,       KC_DEL
 
     ),
